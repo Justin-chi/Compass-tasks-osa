4b04fa613f9d018b4e933d8887a0410c02a74bfa
chigang <chigang@huawei.com>
2017-05-24 11:45:49 +0800

    fix for get openstack code

--- get_openstack_code.sh
+++ get_openstack_code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./sources-branch-updater.sh -o stable/ocata -b stable/ocata -s /opt/git/openstack_services.yml
+/opt/git/sources-branch-updater.sh -o stable/ocata -b stable/ocata -s /opt/git/openstack_services.yml
 find /opt/git/openstack/ -name ".git" | xargs rm -rf
 
